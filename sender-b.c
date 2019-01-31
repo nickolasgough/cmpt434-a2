088be8e0d58bd0e670635d2dd049364024c3a1bf
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-01-31 09:52:21 -0600

    Try the current iteration.

--- sender-b.c
+++ sender-b.c
@@ -152,7 +152,7 @@ int main(int argc, char* argv[]) {
                         break;
                     }
                 }
-                printf("sender-a: acknowledgement for %d successful\n", sNum);
+                printf("sender-a: acknowledgement for %d successful\n", s2Num);
 
                 free(message);
             }
